19b4721ef11c4bda26a9f29eea51c6f2055fe513
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-21 21:44:00 -0300

    feat: add verification if game an category exists

---
+++ src/middlewares/games/checkIfGameAndCategoryExists.js
@@ -0,0 +1,22 @@
+import connection from "../../database/postgres.js";
+
+async function checkIfGameAndCategoryExists(req, res, next) {
+    const { categoryId, name } = req.body;
+    try {
+        const category = await connection.query("SELECT * FROM categories WHERE id = $1;", [categoryId]);
+        const game = await connection.query("SELECT * FROM games WHERE name = $1", [name]);
+        const isCategoryInDB = category.rowCount > 0, 
+              isGameInDB = game.rowCount > 0;
+        if(isCategoryInDB === false) {
+            return res.sendStatus(400);
+        }
+        if(isGameInDB) {
+            return res.sendStatus(409);
+        }
+        next();
+    } catch {
+        res.sendStatus(500);
+    }
+}
+
+export default checkIfGameAndCategoryExists;
