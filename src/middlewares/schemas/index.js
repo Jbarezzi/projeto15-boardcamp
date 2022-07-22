e3ad0221f8ec3bba0bbc27369df0b1a4bd47b4fe
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-21 21:43:01 -0300

    feat: creat games schema

--- src/middlewares/schemas/index.js
+++ src/middlewares/schemas/index.js
@@ -4,4 +4,12 @@ const categorySchema = Joi.object({
     name: Joi.string().trim().required(),
 });
 
-export { categorySchema };
+const gameSchema = Joi.object({
+    name: Joi.string().trim().required(),
+    stockTotal: Joi.number().positive().required(),
+    pricePerDay: Joi.number().positive().required(),
+    image: Joi.string().trim().required(),
+    categoryId: Joi.number().positive().required(),
+});
+
+export { categorySchema, gameSchema };
