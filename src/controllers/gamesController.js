b93e32e8e9e8b652f8c6f668dbfaae1f747a20db
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-22 20:51:36 -0300

    fix: search with query string on games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,20 +1,20 @@
 import connection from "./../database/postgres.js"
 
-async function getGames(_req, res) {
-    // TODO: create search with query string
+async function getGames(req, res) {
+    const { name } = req.query;
     try {
-        const games = await connection.query("SELECT * FROM games;");
-        res.send(games.rows);
+        const query = !!name ? `SELECT * FROM games WHERE name ILIKE '${name}%';`: `SELECT * FROM games;`;
+        const {rows: games} = await connection.query(query);
+        res.send(games);
     } catch {
         res.sendStatus(500);
     }
 }
 
 async function createGame(req, res) {
-    // TODO: fix insert
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        await connection.query("INSERT INTO categories (name,image,stockTotal,categoryId,pricePerDay) VALUES ($1,$2,$3,$4,$5)", [name], [image], [stockTotal], [categoryId], [pricePerDay]);
+        await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',[name,image,stockTotal,categoryId,pricePerDay]);
         res.sendStatus(201);
     } catch {
         res.sendStatus(500);
