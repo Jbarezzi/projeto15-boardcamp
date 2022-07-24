f9a51b86df3291db41f8ad1990b12386554a3f72
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-24 17:32:31 -0300

    fix: querys breaking

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -3,8 +3,9 @@ import connection from "./../database/postgres.js"
 async function getGames(req, res) {
     const { name } = req.query;
     try {
-        const query = !!name ? `SELECT * FROM games WHERE name ILIKE '${name}%';`: `SELECT * FROM games;`;
-        // TODO: implement bind params
+        const filter = !!name ? "WHERE games.name ILIKE $1;" : ";";
+        // TODO: fix filter
+        const query = `SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON categories.id = "categoryId" ${filter}`;
         const {rows: games} = await connection.query(query);
         res.send(games);
     } catch {
@@ -15,8 +16,10 @@ async function getGames(req, res) {
 async function createGame(req, res) {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        const query = ('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',[name,image,stockTotal,categoryId,pricePerDay]);
-        connection.query(query);
+        const query = `INSERT INTO games 
+            (name, image, "stockTotal", "categoryId", "pricePerDay") 
+            VALUES ($1, $2, $3, $4, $5);`
+        connection.query(query, [name,image,stockTotal,categoryId,pricePerDay]);
         res.sendStatus(201);
     } catch {
         res.sendStatus(500);
