4b0570cbb4394f8a02cd1aaf41ac4e386803fb0b
Juan Campos Barezzi <juan.barezzi@gmail.com>
2022-08-04 11:48:01 -0300

    Update postgres.js

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -3,8 +3,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool ({
-    user: "juan",
-    password: "0605",
+    user: "",
+    password: "",
     host: "localhost",
     port: "5432",
     database: "boardcamp",
